803f437f81ce32709b7e9f50f6b83629f98be79a
Ajmal Faris <ajmalfaris.me@gmail.com>
2024-11-13 23:12:02 +0530

    Add logger middleware to root route and implement "hello world" response

--- server.js
+++ server.js
@@ -1,22 +1,28 @@
 // Import the express module
 const express = require('express');
-
 // Initialize an express application
 const app = new express();
 
 // Import the API routes from the 'api' module located in the 'routes' folder
 const api = require('./routes/api');
-
 // Import the admin routes from the 'admin' module located in the 'routes' folder
 const admin = require('./routes/admin');
-
 const contact = require('./routes/contact')
-
 const about = require('./routes/about')
 
-// Define a route for the root URL ('/') that sends a "hello world" response
-app.get('/', (req, res) => {
-    res.send('hello world');
+
+// Define a middleware function named 'logger' that logs a message and calls 'next' to pass control to the next middleware or route handler
+const logger = (req, res, next) => {
+    console.log('Middleware here'); // Log a message to indicate middleware execution
+    next(); // Move to the next middleware/route handler
+};
+
+
+
+// Define a route for the root URL ('/') that uses the 'logger' middleware
+// This route sends a "hello world" response to the client
+app.get('/', logger, (req, res) => {
+    res.send('hello world'); // Respond with "hello world" when the root URL is accessed
 });
 
 
