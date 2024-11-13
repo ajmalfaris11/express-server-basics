3ee00f113ee7dbc0b9f5b031805bc95c6f485bfe
Ajmal Faris <ajmalfaris.me@gmail.com>
2024-11-13 15:38:00 +0530

    Add root route to send "hello world" response

--- server.js
+++ server.js
@@ -4,6 +4,14 @@ const express = require('express');
 // Initialize an express application
 const app = new express();
 
+
+// Define a route for the root URL ('/') that sends a "hello world" response
+app.get('/', (req, res) => {
+    res.send('hello world');
+});
+
+
+
 // Start the server and listen on port 3256
 app.listen(3256, () => {
     console.log('Server is running at port 3256');
